717ff441924d2c17b454372a83a46a5f6951bf29
miceroman <76202940+miceroman@users.noreply.github.com>
2023-03-27 21:55:30 +0300

    rewrote the code a bit
    made the code more beautiful and pleasant to read
    also added the ability to explore multiple accounts, not just one

--- main.py
+++ main.py
@@ -5,106 +5,97 @@ os.system("mode con cols=136 lines=35")
 from bs4 import BeautifulSoup
 
 
-def sleeep(n):
+def strip(q):           #strip
+    print(135 * "─")
+
+def loading(q):         #beautiful loading
+    for i in range(21):
+        time.sleep(0.05)
+        print('\r','Loading',i * s,str(5 * i),'%',end='')
+    print("")
+
+def sleep(q):           #time sleep
     time.sleep(0.25)
 
-def m(q):
-    for p in q:
-        print(p, end='')
-        time.sleep(0.005)
+def logo(q):            #my logo
+    print("""    ─██████──────────██████─██████████─██████████████─██████████████─
+    ─██░░██████████████░░██─██░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─
+    ─██░░░░░░░░░░░░░░░░░░██─████░░████─██░░██████████─██░░██████████─
+    ─██░░██████░░██████░░██───██░░██───██░░██─────────██░░██─────────
+    ─██░░██──██░░██──██░░██───██░░██───██░░██─────────██░░██████████─
+    ─██░░██──██░░██──██░░██───██░░██───██░░██─────────██░░░░░░░░░░██─
+    ─██░░██──██████──██░░██───██░░██───██░░██─────────██░░██████████─
+    ─██░░██──────────██░░██───██░░██───██░░██─────────██░░██─────────
+    ─██░░██──────────██░░██─████░░████─██░░██████████─██░░██████████─
+    ─██░░██──────────██░░██─██░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─
+    ─██████──────────██████─██████████─██████████████─██████████████─""")
 
 
-print(135*"─")
-sleeep(1)
-print("""─██████──────────██████─██████████─██████████████─██████████████─
-─██░░██████████████░░██─██░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─
-─██░░░░░░░░░░░░░░░░░░██─████░░████─██░░██████████─██░░██████████─
-─██░░██████░░██████░░██───██░░██───██░░██─────────██░░██─────────
-─██░░██──██░░██──██░░██───██░░██───██░░██─────────██░░██████████─
-─██░░██──██░░██──██░░██───██░░██───██░░██─────────██░░░░░░░░░░██─
-─██░░██──██████──██░░██───██░░██───██░░██─────────██░░██████████─
-─██░░██──────────██░░██───██░░██───██░░██─────────██░░██─────────
-─██░░██──────────██░░██─████░░████─██░░██████████─██░░██████████─
-─██░░██──────────██░░██─██░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─
-─██████──────────██████─██████████─██████████████─██████████████─""")
-sleeep(1)
-print(135*"─")
-sleeep(1)
-m("Created by https://github.com/miceroman")
-print("")
-sleeep(1)
-print(135*"─")
+strip('')
+
+logo('')
+
+strip('')
+
+sleep('')
+print("Created by https://github.com/miceroman")
+
+strip('')
 
 s='██'
-for i in range(21):
-    time.sleep(0.05)
-    print('\r','Loading',i*s,str(5*i),'%',end='')
 
+loading('')
 
-print("")
-sleeep(1)
-print(135*"─")
+flag = str()
 
-m("Paste the URL to the Steam profile: ")
-url_steam = input()
+while flag != 'N':
+    strip('')
+    print("Paste the URL to the Steam profile: ")
+    url_steam = input()
 
-h = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
-}
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+    }
 
-d = {
-    'name': url_steam
-}
+    data = {
+        'name':url_steam
+    }
 
-r = requests.post('https://faceitfinder.com/profile/', headers=h, data=d)
+    r = requests.post('https://faceitfinder.com/profile/',headers=headers,data=data)
 
-f_id = ''.join([x for x in r.content.decode() if x.isdigit()])
+    f_id = ''.join([x for x in r.content.decode() if x.isdigit()])
 
-r_id = requests.get('https://faceitfinder.com/profile/' + f_id, headers=h)
-r_s = BeautifulSoup(r_id.content, 'html.parser')
-url_steam = r_s.find_all("a", href=True)
+    r_id = requests.get('https://faceitfinder.com/profile/'+f_id,headers=headers)
+    r_s = BeautifulSoup(r_id.content, 'html.parser')
+    url_steam = r_s.find_all("a", href=True)
 
-try:
-    f_url = [x["href"] for x in url_steam if "https://www.faceit.com/" in str(x["href"]) and "csgo" not in str(x["href"])][0]
-    f_stats = r_s.find_all("meta")[::-1][0]["content"]
+    try:
+        f_url = [x["href"] for x in url_steam if "https://www.faceit.com/" in str(x["href"]) and "csgo" not in str(x["href"])][0]
+        f_stats = r_s.find_all("meta")[::-1][0]["content"]
 
-    sleeep(1)
-    print(135 * "─")
+        strip('')
 
-    sleeep(1)
-    m("SteamID: ")
-    m(f_id)
+        print("SteamID: " + str(f_id) + '\n')
+        sleep('')
 
-    print("")
-    print("")
+        print("FaceIT URL: " + str(f_url) + '\n')
+        sleep('')
 
-    sleeep(1)
-    m("FaceIT URL: ")
-    m(f_url)
+        print(str(f_stats))
+        sleep('')
 
-    print(" ")
-    print(" ")
+    except IndexError:                 #if the account was not found
+        strip('')
+        sleep('')
+        print("The player does not have a FaceIT account")
+        sleep('')
 
-    m(f_stats)
+    strip('')
 
-except IndexError:
-    print(135 * "─")
-    m("The player does not have a FaceIT account")
-    print(" ")
+    print("To explore another account, press ENTER")
+    sleep('')
 
+    print("To close the application, print N")
+    sleep('')
 
-
-sleeep(1)
-print("")
-
-sleeep(1)
-print(135*"─")
-
-sleeep(1)
-print("")
-print("")
-sleeep(1)
-
-m("Press ENTER to close the window...")
-
-stop = input()
+    flag = input()
